c4d59fcd2b28e7f90a849bd7382335b2f6f073d4
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-04-05 12:45:21 -0300

    Nova classe

---
+++ src/main/java/com/uniskill/uniskill_api/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.uniskill.uniskill_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean // quando alguém precisar de um password encorder usa este
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
