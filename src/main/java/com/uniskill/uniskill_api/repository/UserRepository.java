356a703055c4ad7a9a29f5418797a193751bbefc
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-04-05 12:44:39 -0300

    Novo metodo adicionado para buscar por email

--- src/main/java/com/uniskill/uniskill_api/repository/UserRepository.java
+++ src/main/java/com/uniskill/uniskill_api/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.uniskill.uniskill_api.repository;
 
 import com.uniskill.uniskill_api.model.User;
+import org.hibernate.dialect.lock.OptimisticEntityLockException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
