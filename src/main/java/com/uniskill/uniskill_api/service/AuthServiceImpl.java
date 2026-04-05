9ce4872b587452835769059c2f1113e914bee372
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-04-05 12:46:09 -0300

    Metodos de autenticação adicionados

--- src/main/java/com/uniskill/uniskill_api/service/AuthServiceImpl.java
+++ src/main/java/com/uniskill/uniskill_api/service/AuthServiceImpl.java
@@ -3,22 +3,57 @@ package com.uniskill.uniskill_api.service;
 import com.uniskill.uniskill_api.dto.LoginRequestDTO;
 import com.uniskill.uniskill_api.dto.LoginResponseDTO;
 import com.uniskill.uniskill_api.dto.RegisterRequestDTO;
+import com.uniskill.uniskill_api.model.User;
 import com.uniskill.uniskill_api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor // gera o construtor automaticamente e o Spring injeta a dependência
 public class AuthServiceImpl implements AuthService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void register(RegisterRequestDTO registerRequestDTO) {
+        //verifica se o email já existe
+        if(userRepository.findByEmail(registerRequestDTO.getEmail()).isPresent()){
+            throw new RuntimeException("Email já cadastrado");
+        }
+        //criptografa a senha com BCrypt
+        String encryptedPassword = passwordEncoder.encode(registerRequestDTO.getPassword());
 
+        //cria o objeto user e preenche todos os campos
+        User user = new User();
+
+        user.setUsername(registerRequestDTO.getName());
+        user.setEmail(registerRequestDTO.getEmail());
+        user.setPassword(encryptedPassword);
+        user.setCreatedAt(LocalDateTime.now());
+        user.setRole(registerRequestDTO.getRole());
+        user.setPhone(registerRequestDTO.getPhone());
+
+        //salva no banco de dados
+        userRepository.save(user);
     }
 
     @Override
     public LoginResponseDTO login(LoginRequestDTO loginRequestDTO) {
+        Optional<User> optionalUser = userRepository.findByEmail(loginRequestDTO.getEmail());
+        if (!optionalUser.isPresent()){
+            throw new RuntimeException("Email não encontrado!");
+        }
+
+        User user = optionalUser.get();
+
+        if (!passwordEncoder.matches(loginRequestDTO.getPassword(), user.getPassword())){
+           throw new RuntimeException("Senha incorreta!");
+        }
+
         return null;
     }
 }
