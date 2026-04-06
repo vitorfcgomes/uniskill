24d24946ef522770c2a69dc7808bdae9d8a708c9
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-04-06 18:19:52 -0300

    Novos Beans adicionados

--- src/main/java/com/uniskill/uniskill_api/config/SecurityConfig.java
+++ src/main/java/com/uniskill/uniskill_api/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.uniskill.uniskill_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +14,19 @@ public class SecurityConfig {
     @Bean // quando alguém precisar de um password encorder usa este
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+        return http.build();
     }
 }
