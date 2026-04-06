e8a0edd66aa36248a583b62348dcc7a8cf112efa
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-04-06 18:19:11 -0300

    Entendendo Autenticação

--- src/main/java/com/uniskill/uniskill_api/service/JwtService.java
+++ src/main/java/com/uniskill/uniskill_api/service/JwtService.java
@@ -1,8 +1,14 @@
 package com.uniskill.uniskill_api.service;
 
 import com.uniskill.uniskill_api.model.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Service
 
@@ -14,10 +20,32 @@ public class JwtService {
     @Value("${jwt.expiration}") //injeta o tempo de expiração do token
     private Long expiration;
 
+
     public String generateToken(User user){
-        return null;
+        Date expirationDate = new Date(System.currentTimeMillis() + expiration); //Isso pega o momento atual em milissegundos e
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .claim("role", user.getRole())
+                .issuedAt(new Date())
+                .expiration(expirationDate)
+                .signWith(getSigningKey())
+                .compact();                                                                      // soma o tempo de expiração que você definiu no application.properties — resultando na data exata em que o token vai expirar.
+
     }
+
     public boolean validateToken(String token){
-        return false;
+        try{
+            Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token);
+            return true;
+
+        }catch(Exception e){
+                    return false;
+        }
+    }
+
+    //metódo que converte uma string secreta em uma chave criptográfica
+    private SecretKey getSigningKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
