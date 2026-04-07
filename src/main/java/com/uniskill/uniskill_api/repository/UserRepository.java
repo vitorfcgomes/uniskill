ac1f0e5b0228d951525f3177385b739c9b9e76b6
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-04-07 17:00:37 -0300

    refactor: melhoria na implementação

--- src/main/java/com/uniskill/uniskill_api/repository/UserRepository.java
+++ src/main/java/com/uniskill/uniskill_api/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.uniskill.uniskill_api.repository;
 
 import com.uniskill.uniskill_api.model.User;
-import org.hibernate.dialect.lock.OptimisticEntityLockException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
