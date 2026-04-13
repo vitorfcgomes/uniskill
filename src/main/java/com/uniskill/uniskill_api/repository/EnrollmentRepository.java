09f9f5a29fe9d281a0d8c49eccd29f69b1e82893
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-04-12 21:04:28 -0300

    feat: implementando método para verificar se o usuário existe ou não

--- src/main/java/com/uniskill/uniskill_api/repository/EnrollmentRepository.java
+++ src/main/java/com/uniskill/uniskill_api/repository/EnrollmentRepository.java
@@ -1,9 +1,12 @@
 package com.uniskill.uniskill_api.repository;
 
+import com.uniskill.uniskill_api.model.Course;
 import com.uniskill.uniskill_api.model.Enrollment;
+import com.uniskill.uniskill_api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, UUID> {
+    public boolean existsByUserAndCourse(User user, Course course);
 }
