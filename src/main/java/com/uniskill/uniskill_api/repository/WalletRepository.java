ee179aca5b705dc901d13f07e68cc72a8a08805a
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-04-13 23:05:30 -0300

    delete: walletrepository

--- src/main/java/com/uniskill/uniskill_api/repository/WalletRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uniskill.uniskill_api.repository;
-
-import com.uniskill.uniskill_api.model.Wallet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface WalletRepository extends JpaRepository<Wallet, UUID> {
-}
